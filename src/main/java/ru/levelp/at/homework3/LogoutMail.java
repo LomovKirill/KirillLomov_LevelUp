36222f11ad2d0fbd53c8fcd8e33bb122941935b8
LomovKirill <Heskey41@gmail.com>
2022-10-13 00:14:56 +0300

    add new classes, refactoring test

---
+++ src/main/java/ru/levelp/at/homework3/LogoutMail.java
@@ -0,0 +1,36 @@
+package ru.levelp.at.homework3;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class LogoutMail {
+
+    private final WebDriver driver;
+    private final WebDriverWait wait;
+
+    @FindBy(xpath = "//*[@data-testid='whiteline-account']")
+    private WebElement openLogin;
+
+    @FindBy(xpath = "//*[@data-testid='whiteline-account-exit']")
+    private WebElement exit;
+
+    public LogoutMail(WebDriver driver) {
+        this.driver = driver;
+        wait = new WebDriverWait(driver, Duration.ofMillis(1000));
+        PageFactory.initElements(driver, this);
+    }
+
+    public void openLogin() {
+        wait.until(ExpectedConditions.elementToBeClickable(openLogin)).click();
+    }
+
+    public void exit() {
+        wait.until(ExpectedConditions.visibilityOf(exit)).click();
+    }
+}
--- src/test/java/ru/levelp/at/homework3/SendMailTest.java
+++ src/test/java/ru/levelp/at/homework3/SendMailTest.java
@@ -1,46 +1,52 @@
 package ru.levelp.at.homework3;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.not;
 
-public class SendMailTest {
-    protected static final String MAIL_RU_URL = "https://mail.ru/";
+public class SendMailTest extends BaseTest {
 
-    protected WebDriver driver;
-
-    @BeforeEach
-    public void setUp() {
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        driver.navigate().to(MAIL_RU_URL);
-        driver.manage().window().maximize();
-        SleepUtils.sleep(1500);
-    }
+    String subject = "Два дня тебя искал";
+    String body = "А дело было в wait, обидно";
 
     @Test
-    public void creatingAndSavingAndSendingLetter() {
-        driver.findElement(By.xpath("//*[@class=\"ph-login svelte-1hiqrvn\"]")).click();
-        SleepUtils.sleep(1000);
-        WebElement iFrame = driver.findElement(By.cssSelector("iframe.ag-popup__frame__layout__iframe"));
-        SleepUtils.sleep(1500);
-        driver.switchTo().frame(iFrame);
-        SleepUtils.sleep(1500);
-        driver.findElement(By.xpath("//*[@placeholder=\"Имя аккаунта\"]")).sendKeys("klomovtest");
-        driver.findElement(By.xpath("//*[@class=\"submit-button-wrap\"]")).click();
-        SleepUtils.sleep(100);
-        driver.findElement(By.xpath("//*[@placeholder=\"Пароль\"]")).sendKeys("TestPassword123");
-        driver.findElement(By.xpath("//*[@class=\"submit-button-wrap\"]")).click();
-        SleepUtils.sleep(3500);
+    public void creatingAndSavingAndSendingEmail() {
+
+        var loginMail = new LoginMail(driver);
+        loginMail.openLogin();
+        loginMail.switchFrame();
+        loginMail.nameField(login);
+        loginMail.clickLogin();
+        loginMail.passwordField(password);
+        loginMail.clickLogin();
+
+        SleepUtils.sleep(4000);
         assertThat(driver.getTitle()).contains("Входящие");
         driver.findElement(By.xpath("//*[@data-click-counter=\"116987833\"]")).click();
-        driver.findElement(By.xpath("//*[@href=\"/compose/\"]")).click();
-        driver.findElement(By.xpath("//*[@class=\"container--H9L5q size_s--3_M-_\"]")).sendKeys("klomovtest@mail.ru");
-        SleepUtils.sleep(1500);
+
+        var createMail = new CreateEmail(driver);
+        createMail.clickWriteEmail();
+        createMail.fieldTo(email);
+        createMail.fieldSubject(subject);
+        createMail.fieldBody(body);
+        createMail.clickSave();
+        createMail.clickClose();
+
+        driver.findElement(By.linkText("Черновики")).click();
+        assertThat(driver.getPageSource().contains(email)).isEqualTo(true);
+        assertThat(driver.getPageSource().contains(subject)).isEqualTo(true);
+        assertThat(driver.getPageSource().contains(body)).isEqualTo(true);
+
+        driver.findElement(By.xpath("//div[@role='rowgroup']/a[1]")).click();
+        createMail.clickSend();
+        SleepUtils.sleep(2000);
+        createMail.clickCloseAfter();
+
+        driver.findElement(By.linkText("Отправленные")).click();
+
+        var logoutMail = new LogoutMail(driver);
+        logoutMail.openLogin();
+        logoutMail.exit();
     }
 }
